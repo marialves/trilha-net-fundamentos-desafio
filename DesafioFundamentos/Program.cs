ffed9029773ea4cdc51c9eea8ec2168d02efa667
Mariana Alves <pmarianaaq@hotmail.com>
2023-11-01 15:04:12 -0300

    Desafio estacionamento concluído!

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,6 +6,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
+Console.Clear();
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
